8a587f9eea672ccb1052633d8390a30560fa2cb6
questerzxy <82453776+questerzxy@users.noreply.github.com>
2022-07-21 15:15:08 +0800

    Update vertical.vue

--- docs/examples/menu/vertical.vue
+++ docs/examples/menu/vertical.vue
@@ -83,18 +83,3 @@
     </el-col>
   </el-row>
 </template>
-
-<script lang="ts" setup>
-import {
-  Document,
-  Menu as IconMenu,
-  Location,
-  Setting,
-} from '@element-plus/icons-vue'
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-</script>
